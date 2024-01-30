c116c533e49413fd791831890b905e7cfda02d6d
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-01-30 15:36:56 -0500

    Updated routes for controller action managment

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,41 +1,20 @@
-const router = require('express').Router();
-const { v4 } = require("uuid");
-const UsersData = require('../data/user');
-const { json } = require('express');
+const router = require("express").Router();
+const {
+  getAllUsers,
+  getOneUser,
+  createNewUser,
+  deleteUser,
+  updateUser
+} = require("../controllers/usersController");
 
-router.get('/', (req, res) => {
-  const usersData = new UsersData();
-  console.log(UsersData);
-  const users = usersData.getUsers();
-  res.json(users);
-});
+router.get("/", getAllUsers,);
 
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  const usersData = new UsersData();
-  const user = usersData.getUserById(Number(id));
-  res.json(user);
-});
+router.get("/:id", getOneUser,);
 
-router.post('/',(req,res)=>{
-  const usersData = new UsersData;
-  const newUser =  usersData.createUser(v4(),req.body);
-  console.log(usersData.datos);
-  res.json(newUser);
-});
+router.post("/", createNewUser,);
 
-router.delete('/:id',(req,res)=>{
-  const usersData = new UsersData;
-  const userDeleted = usersData.deleteUser(Number(req.params.id));
-  console.log(usersData.datos);
-  res.json(userDeleted);
-})
+router.delete("/:id", deleteUser,);
 
-router.put('/:id',(req,res)=>{
-  const usersData = new UsersData;
-  const userUpdated = usersData.updateUser(Number(req.params.id), req.body);
-  console.log(userUpdated);
-  res.json(userUpdated);
-})
+router.put("/:id",updateUser,);
 
 module.exports = router;
