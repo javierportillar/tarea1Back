96ecd1df4ebe8f10c1533e28d5166f695619c8af
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-04 10:37:11 -0500

    deleted v4 import, removed ChekBodyJson class, from gettAllUsers it was deleted empty array error, uptaded from createNewUser ChekBodyJson.validateCreateUser because class was modified, from updateUser it was modified status code response, from 400 to 404

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,40 +1,10 @@
 const { UsersData, message } = require("../data/user");
-const { v4 } = require("uuid");
+const ChekBodyJson = require("../data/chekBodyInput");
 const usersData = new UsersData();
 const msjData = new message();
 
-class ChekBodyJson {
-  static validateUser(data) {
-    //Validate that req.Body Only have id,name and age as keys
-    const errors = [];
-    const validInputs = ["id", "name", "age"];
-    const dataKeys = Object.keys(data);
-    dataKeys.forEach((key) => {
-      if (!validInputs.includes(key)) {
-        errors.push(`Propiedad inválida: ${key}`);
-      }
-    });
-    // Validate that ID and NAME are an String
-    if (typeof data.id !== "string") {
-      console.log(typeof data.id);
-      errors.push("id must be string");
-    }
-    if (typeof data.name !== "string") {
-      errors.push("name must be string");
-    }
-    // Validate that AGE is an number
-    if (typeof data.age !== "number" || !Number.isInteger(data.age)) {
-      errors.push("age must be numero");
-    }
-    return errors;
-  }
-}
-
 const getAllUsers = (req, res, next) => {
   const users = usersData.getUsers();
-  if (users.length == 0) {
-    return res.status(200).send(msjData.msj("No se encontraron usuarios", 200));
-  }
   return res.status(200).json(users);
 };
 
@@ -58,22 +28,23 @@ const deleteUser = (req, res, next) => {
 };
 
 const createNewUser = (req, res, next) => {
-  const errors = ChekBodyJson.validateUser(req.body);
+  const errors = ChekBodyJson.validateCreateUser(req.body);
   if (errors.length > 0) {
-    res.status(400).json(errors); //Se eliminó return para que al momento de no poner id, salga el error pero creé el obj con id v4()
+    return res.status(400).json(errors);
   }
-  const newUser = usersData.createUser(v4(), req.body);
+  const newUser = usersData.createUser(req.body);
   return res.json(newUser);
 };
 
 const updateUser = (req, res, next) => {
-  const errors = ChekBodyJson.validateUser(req.body);
+  const idJSON = req.params;
+  const errors = ChekBodyJson.validateUser(idJSON, req.body);
   if (errors.length > 0) {
     return res.status(400).json(errors);
   }
-  const userUpdated = usersData.updateUser(Number(req.params.id), req.body);
+  const userUpdated = usersData.updateUser(req.params.id, req.body);
   if (!userUpdated) {
-    return res.status(400).send(msjData.msj('Usuario No encontrado','400'));
+    return res.status(404).send(msjData.msj("Usuario No encontrado", "400"));
   }
   return res.json(userUpdated);
 };
