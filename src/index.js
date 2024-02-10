a26fb009c866493cad9017df5e509d78868a00a8
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-02-09 19:35:12 -0500

    added cors middleware and setted with port direction request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = require('./routes/userRoutes');
+const cors = require('cors')
 const app = express();
 const port = 4400;
 
 app.use(express.json());
+app.use(cors({origin: 'http://localhost:5173',
+optionsSuccessStatus: 200
+}));
 app.use('/users', router);
 
 app.listen(port, () => {
